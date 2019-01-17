0cd5ea6d45bb7ca8f1beb9131937f125e7a18b8a
Daniel Brown <dsbrown1331@gmail.com>
2019-01-17 14:38:51 -0600

    custom reward

--- baselines/common/custom_reward_wrapper.py
+++ baselines/common/custom_reward_wrapper.py
@@ -14,7 +14,8 @@ class AtariNet(nn.Module):
         self.conv2 = nn.Conv2d(16, 16, 5, stride=2)
         self.conv3 = nn.Conv2d(16, 16, 3, stride=1)
         self.conv4 = nn.Conv2d(16, 16, 3, stride=1)
-        self.fc1 = nn.Linear(1936, 64)
+        #self.fc1 = nn.Linear(1936, 64)
+        self.fc1 = nn.Linear(784, 64)
         self.fc2 = nn.Linear(64, 1)
 
 
@@ -26,9 +27,12 @@ class AtariNet(nn.Module):
             #compute forward pass of reward network
             x = F.leaky_relu(self.conv1(x))
             x = F.leaky_relu(self.conv2(x))
-            x = x.view(-1, 1936)
+            x = F.leaky_relu(self.conv3(x))
+            x = F.leaky_relu(self.conv4(x))
+            x = x.view(-1, 784)
+            #x = x.view(-1, 1936)
             x = F.leaky_relu(self.fc1(x))
-            r = torch.sigmoid(self.fc2(x)) #clip reward?
+            r = torch.tanh(self.fc2(x)) #clip reward?
             sum_rewards += r
         ##    y = self.scalar(torch.ones(1))
         ##    sum_rewards += y
